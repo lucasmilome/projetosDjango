eeb7325c9c749bb9198434eff4b71e7be794c043
lucasmilome <lucashenriquejafra@gmail.com>
2025-07-01 15:01:28 -0400

    01/07 commit

--- projetos/admin.py
+++ projetos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from projetos.models import Projeto
 
 # Register your models here.
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ('titulo_alias', 'descricao_alias', 'tecnologia_alias')
+    list_filter = ('tecnologia',)
+    ordering = ('tecnologia',)
+    search_fields = ('titulo',)
+
+    @admin.display(description='Título')
+    def titulo_alias(self, obj):
+        return obj.titulo
+    
+    @admin.display(description='Descrição')
+    def descricao_alias(self,obj):
+        return obj.descricao
+    
+    @admin.display(description='Tecnologia')
+    def tecnologia_alias(self,obj):
+        return obj.tecnologia
--- projetos/models.py
+++ projetos/models.py
@@ -5,4 +5,7 @@ class Projeto(models.Model):
     titulo = models.CharField(max_length=180)
     descricao = models.TextField()
     tecnologia = models.CharField(max_length=20)
-    imagem = models.FilePathField(path='/img')
+    imagem = models.FileField(upload_to='imagens_projetos', blank=True)
+
+    def __str__(self):
+        return self.titulo.title()
