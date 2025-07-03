7f742b1192091cc90d099988c4c794ddc0c04bac
lucasmilome <lucashenriquejafra@gmail.com>
2025-07-03 16:42:54 -0400

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from blog.models import Postagem, Categoria
 
 # Create your views here.
+def blog_index(request):
+    postagens = Postagem.objects.all().order_by('-criado_em')
+
+    context = {
+        'postagens': postagens, 
+    }
+
+    return render(request, 'blog/blog_index.html', context)
+
+def blog_categoria(request, categoria):
+    postagens = Postagem.objects.filter(
+        categorias_name_contains=categoria
+    ).order_by('-criado_em')
+
+    context = {
+        'categoria': categoria,
+        'postagens': postagens,
+    }
+    
+    return render(request, 'blog/blog_categoria.html', context)
