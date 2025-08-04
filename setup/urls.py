0844def1642ab69e67941502f61fd102348227fa
lucasmilome <lucashenriquejafra@gmail.com>
2025-08-04 13:38:21 -0400

    first commit

--- setup/urls.py
+++ setup/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('conta/', include('conta.urls')),
     path('projetos/', include('projetos.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
