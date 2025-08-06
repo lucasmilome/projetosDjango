07f00c2790eeeea3be56518f812e05e843b8eb70
lucasmilome <lucashenriquejafra@gmail.com>
2025-08-06 13:46:05 -0400

    first commit

--- conta/views.py
+++ conta/views.py
@@ -1,38 +1,75 @@
 from django.shortcuts import render, redirect
 from conta.forms import LoginForm, RegisterForm
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+
 
 # Create your views here.
 def login(request):
     form = LoginForm()
-    return render(request, 'conta/login.html', {'form': form})
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+    if form.is_valid():
+        nome_usuario = form["login"].value().strip()
+        senha = form["password"].value().strip()
+
+        if nome_usuario == "" or senha == "":
+            messages.error(request, "Todos os campos são obrigatórios!")
+            return render(request, "conta/login.html", {"form": form})
+
+        credenciais_usuario = auth.authenticate(
+            request,
+            username=nome_usuario,
+            password=senha,
+        )
+
+        if credenciais_usuario == None:
+            messages.error(request, "Login falho! Tente novamente")
+            return render(request, "conta/login.html", {"form": form})
+
+        auth.login(request, credenciais_usuario)
+        return redirect("projeto_index")
+
+    return render(request, "conta/login.html", {"form": form})
+
 
 def register(request):
     form = RegisterForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            nome_usuario = form['login'].value().strip()
-            email_usuario = form['email'].value().strip()
-            senha = form['password'].value().strip()
-            confirma_senha = form['reenter_password'].value().strip()
+            nome_usuario = form["login"].value().strip()
+            email_usuario = form["email"].value().strip()
+            senha = form["password"].value().strip()
+            confirma_senha = form["reenter_password"].value().strip()
 
-            if nome_usuario == '' or email_usuario == '' or senha == '' or confirma_senha == '':
-                return redirect('register')
-            
+            if (
+                nome_usuario == ""
+                or email_usuario == ""
+                or senha == ""
+                or confirma_senha == ""
+            ):
+                return redirect("register")
+
             if senha != confirma_senha:
-                return redirect('register')
-            
+                messages.error(request, "As senhas não coincidem!")
+                return render(request, "conta/register.html", {"form": form})
+
             user_exists = User.objects.filter(username=nome_usuario).exists()
             email_exists = User.objects.filter(email=email_usuario).exists()
 
             if user_exists:
-                return redirect('register')
-            
+                messages.error(request, "Já existe um usúario com este nome")
+                return render(request, "conta/register.html", {"form": form})
+
             if email_exists:
-                return redirect('register')
+                messages.error(request, "Esse e-mail já está cadastrado!")
+                return render(request, "conta/register.html", {"form": form})
 
             new_user = User.objects.create_user(
                 username=nome_usuario,
@@ -41,6 +78,7 @@ def register(request):
             )
 
             new_user.save()
-            return redirect('login')
-        
-    return render(request, 'conta/register.html', {'form': form})
+
+            messages.success(request, "Usuário cadastrado com sucesso!")
+
+    return render(request, "conta/register.html", {"form": form})
